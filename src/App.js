4286f9459e9673cea62a8043fca3d4c5dee15bdc
SushmaSathish <sushmasathish.ngl@gmail.com>
2023-09-13 11:50:29 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,32 +1,77 @@
-import React from 'react';
-import './App.css';
-//import DUMMY from './components/cart/dummy';
-//import Cart from './components/cart/cart';
-//import { Route ,Switch} from  'react-router-dom';
-import HeaderForAbout from './components/pages/headerForAbout';
+import React from "react";
+import { Route, Switch, useHistory } from "react-router-dom";
 
-import HomeBody from './components/pages/Homebody';
-/*import FooterHome from './components/pages/footerHome';*/
-import Contact from './components/pages/Contact';
-//import Appapi from './components/AppAPI/AppAPI';
+import Product1 from "./components/productjs/product1";
+import Product2 from "./components/productjs/product2";
+import Product3 from "./components/productjs/product3";
+import Product4 from "./components/productjs/product4";
+import Home from "./Pages/home";
+import HeaderForAbout from "./Pages/Headerforabout";
+import Store from "./components/itemsavailable/dummy";
+import ContactUs from "./Pages/contact";
+import AuthForm from "./Auth/auth-form";
+import { useContext } from "react";
+import AuthContext from "./Auth/auth-context";
+import { useEffect } from "react";
 
-import {Route} from 'react-router-dom';
-
-
-let App=()=> {
+function App() {
+  const authCtx = useContext(AuthContext);
+  const history = useHistory();
+  useEffect(() => {
+    if (!authCtx.isLoggedIn) {
+      console.log(authCtx.isLoggedIn)
+      history.replace("/");
+    }
+  }, [authCtx.isLoggedIn, history]);
   return (
-   <div>  
-     <Route path='/about'>
-      <HeaderForAbout/>
-     </Route> 
-     <Route path='/contact'>
-      < Contact/>
-     </Route>  
-     <Route path=' '>
-      <HomeBody/>
-     </Route>     
-   </div>
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AuthForm />
+        </Route>
+        {authCtx.isLoggedIn && (
+          <Route path="/home">
+            <Home />
+          </Route>
+        )}
+        {authCtx.isLoggedIn && (
+          <Route path="/about">
+            <HeaderForAbout />
+          </Route>
+        )}
+        {authCtx.isLoggedIn && (
+          <Route path="/store"exact>
+            <Store />
+          </Route>
+        )}
+
+        {authCtx.isLoggedIn && (
+          <Route path="/store2">
+            <Product1 />
+          </Route>
+        )}
+        {authCtx.isLoggedIn && (
+          <Route path="/store1">
+            <Product2 />
+          </Route>
+        )}
+        {authCtx.isLoggedIn && (
+          <Route path="/store3">
+            <Product3 />
+          </Route>
+        )}
+        {authCtx.isLoggedIn && (
+          <Route path="/store4">
+            <Product4 />
+          </Route>
+        )}
+
+        {authCtx.isLoggedIn && (
+          <Route path="/contact">
+            <ContactUs />
+          </Route>
+        )}
+      </Switch>
+    </div>
   );
 }
-
-export default App;
--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -1,10 +1,18 @@
 import Container from "react-bootstrap/Container";
-import { Navbar,Nav } from "react-bootstrap";
+import { Navbar,Nav ,Button} from "react-bootstrap";
 import CartButton from './navbarcartbutton';
-import {NavLink } from "react-router-dom";
+import {NavLink ,useHistory} from "react-router-dom";
 import classes from './navbar.module.css';
+import { useContext } from "react";
+import AuthContext from "../Auth/Authcontext";
 
 const Navigationbar=(props)=>{
+  const authCtx = useContext(AuthContext);
+
+const history=useHistory()
+  const isLoggedIn = authCtx.isLoggedIn;
+  const logoutHandler = () =>{authCtx.logout();
+    history.replace("/");}
     return(
         <div>
         <Navbar className={classes.header}>
@@ -39,6 +47,17 @@ const Navigationbar=(props)=>{
               </th>
             </Nav>
            <CartButton onButtonpass={props.onOpenButton} />
+           <Nav>
+       {isLoggedIn && (
+            <Button onClick={logoutHandler}variant="info"
+            style={{ border: "1px solid skyblue",
+            marginLeft: "10px", }}
+            type="click">
+              Logout
+            </Button>
+
+          )}
+          </Nav>
            </Container>
           </Navbar>
         </div>
